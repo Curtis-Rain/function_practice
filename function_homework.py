5059fdca7ef3c883bbee0bd7c773cde4151fd6a6
Jokerflower <2328316809@qq.com>
2018-06-06 20:49:19 +0800

    hi

---
+++ function_homework.py
@@ -0,0 +1,45 @@
+def get_income(hot_dog):
+	income = (6 * hot_dog) - 3000
+	return income
+
+print(get_income(400))
+print(get_income(400))
+
+
+
+def foo(n):
+	num = (6 * n) - 4
+	return num
+
+print(foo(4))
+print(foo(5))
+print(foo(6))
+print(foo(7))
+
+
+
+
+def coo(a):
+	num = (1 / (3 ** a - 1))
+	return num
+
+
+print(coo(1))
+print(coo(2))
+print(coo(3))
+print(coo(4))
+print(coo(5))
+
+
+
+def func(c):
+	num = (-1 ** c) * (3)
+	return num
+
+
+
+print(func(1))
+print(func(2))
+print(func(3))
+print(func(4))
+print(func(5))
